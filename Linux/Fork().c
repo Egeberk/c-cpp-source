4958ecd094ff944c71e5c663ff179ab86ac0861c
Egeberk Özberk <egeberk@gmail.com>
2017-11-21 01:23:43 +0300

    Update Fork().c

--- Linux/Fork().c
+++ Linux/Fork().c
@@ -1,22 +1,6 @@
 //////////////////////////
 // Fork permute program //
-//////////////////////////
-/*
- * Answer 1) 
- * When main process(parent) arrives first fork() , creates a child process. Lets call it c1. 
- * Then it goes into if statement and calls swap(0,1) while its parent enters else and waits the child.
- * After that both c1 and its parent arrives another fork(), both creating a child process.
- * Now we have 4 processes. Lets call them c1's child c2, and main process' child c3.
- * Both c2 and c3 will go into the if statement as they are now 'new childs', while parent (still main process) and c1 enters the else statement.
- * Child of c1 (c2) will call swap(0,2) modifying the array changed by its parent. And c3 does the same as it calles swap(0,2). Their job is done for swaps.
- * Now the parents (main and c1) will go into the else statement waiting for others. Then they both arrive 3rd fork().
- * This will create a child both for c1 and main. Lets call main's new child c4 and c1's new child c5.
- * After this event new childs enter the if statement and both call swap(1,2), modifying their parents arrays.
- * Parents go into the else and waits for all.
- * As a result we have 6 processes, (main-c1-c2-c3-c4-c5) both modified and array for their own thus we have 6 different arrays.
- * Each of the processes have one more job which is printing their own arrays.
- */
-// Answer 2) 
+////////////////////////// 
 
 ///////////////////////////////////////////////////////////////////////////////////////
 //     THIS PART IS NECESSARY TO MODIFY THE PROGRAM OUTPUT IN ORDER TO EVALUATE      //
